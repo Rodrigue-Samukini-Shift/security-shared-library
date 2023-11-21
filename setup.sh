5f9c2d78e81b6dfa4a9e736729decc1b14925c66
Rodrigue Sesanga Samukini <rodrigue.samukini@shift-technology.com>
2023-11-21 10:46:59 +0100

    updates files

--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@ curl --create-dirs -o .github/actions/create-sandbox/action.yml https://raw.gith
 curl --create-dirs -o secu-requirements.txt https://raw.githubusercontent.com/Rodrigue-Samukini-Shift/security-shared-library/main/requirements.txt >> security.log
 curl --create-dirs -o create-application-version.py https://raw.githubusercontent.com/Rodrigue-Samukini-Shift/security-shared-library/main/src/create-application-version.py >> security.log
 mkdir -p ../../../.veracode >> security.log
-export VERACODE_API_KEY_ID= $1
-export VERACODE_API_KEY_SECRET= $2
 echo [default] > ../../../.veracode/credentials >> security.log
-echo veracode_api_key_id = $1 >> ../../../.veracode/credentials
-echo veracode_api_key_secret = $2 >> ../../../.veracode/credentials
+echo \"veracode_api_key_id = $1\" >> ../../../.veracode/credentials
+echo \"veracode_api_key_secret = $2\" >> ../../../.veracode/credentials
