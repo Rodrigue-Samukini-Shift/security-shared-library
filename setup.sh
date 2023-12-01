8b66a391b00235fcf918b7f42dc293b9ca7c86ae
Rodrigue Sesanga Samukini <rodrigue.samukini@shift-technology.com>
2023-12-01 10:03:32 +0100

    fix syntax error in the scipt

--- setup.sh
+++ setup.sh
@@ -7,7 +7,4 @@ mkdir -p ../../../.veracode >> security.log
 echo [default] > ../../../.veracode/credentials
 echo veracode_api_key_id = $1 >> ../../../.veracode/credentials
 echo veracode_api_key_secret = $2 >> ../../../.veracode/credentials
-find . -name '*.dll'
-#zip scan-files.zip "${@:3}" >> security.log
-zip scan-files.zip $(find . -name '*.dll') >> security.log
-cat security.log
+zip scan-files.zip "${@:3}" >> security.log
